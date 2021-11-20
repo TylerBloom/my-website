76b3230083cb1d07c3df4b60b3918177708cdb13
TylerBloom <tylerbloom2222@gmail.com>
2021-11-20 14:59:57 -0500

    Figuring things out.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 
 #[get("/")]
 async fn root() -> Result<NamedFile, NotFound<String>> {
-    NamedFile::open("site/static/index.html")
+    NamedFile::open("site/static/first.html")
         .await
         .map_err(|e| NotFound(e.to_string()))
 }
@@ -15,7 +15,6 @@ async fn root() -> Result<NamedFile, NotFound<String>> {
 #[get("/<path..>")]
 async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
     let path = PathBuf::from("site").join(path);
-    println!( "{:?}", path );
     NamedFile::open(path).await.map_err(|e| NotFound(e.to_string()))
 }
 
