32ec5f9444f2e285a8b1a7fae3f9198f482d5544
TylerBloom <tylerbloom2222@gmail.com>
2021-11-22 01:43:10 -0500

    Workaround found. Adding templating back in

--- src/blog.rs
+++ src/blog.rs
@@ -1,35 +1,52 @@
 
-use rocket::response::content;
+//use rocket::response::Redirect;
+
+use rocket_dyn_templates::Template;//, tera::Tera, context};
+use serde::Serialize;
 
 use std::fs;
+use std::path::PathBuf;
 
-use super::utils::{construct_html_from_base, IconGrid};
+struct BlogIcon {
+    pub html: String,
+}
+
+#[derive(Serialize)]
+struct IconRows {
+    pub rows: Vec<String>,
+}
+
 
 
 #[get("/")]
-pub fn index() -> content::Html<String> {
-    let posts = IconGrid::load( fs::read_dir("site/blog/posts").unwrap().into_iter()
-                .map(|f| {f.unwrap().path().display().to_string()} ).collect() );
-    content::Html( construct_html_from_base( posts.export_to_grid() ) )
+pub fn index() -> Template {
+    let mut icons: Vec<BlogIcon> = Vec::new();
+    for dir in fs::read_dir("site/blog/posts").unwrap() {
+        match BlogIcon::load_from_dir(dir.unwrap().path()) {
+            Some(icon) => { icons.push(icon); },
+            None => {},
+        }
+    }
+    let mut posts = IconRows { rows : Vec::new() };
+    let mut row = String::new();
+    for (i, icon) in icons.iter().enumerate() {
+        if i != 0 && i % 3 == 0 {
+            posts.rows.push(row.clone());
+            row = String::new();
+        }
+        row += &icon.html;
+    }
+    posts.rows.push(row.clone());
+    Template::render("blog/index", posts )
 }
 
-/*
-#[catch(404)]
-pub fn not_found(req: &Request<'_>) -> Template {
-    Template::render("tera/error/404", context! {
-        uri: req.uri()
-    })
-}
 
-pub fn customize(tera: &mut Tera) {
-    tera.add_raw_template("blog/index.html", r#"
-        {% extends "tera/base" %}
-
-        {% block content %}
-            <section id="about">
-              <h1>About - Here's another page!</h1>
-            </section>
-        {% endblock content %}
-    "#).expect("valid Tera template");
+impl BlogIcon {
+    pub fn load_from_dir(mut dir: PathBuf) -> Option<Self> {
+        dir.push("icon.html");
+        match fs::read_to_string(dir.as_path().to_str().unwrap()) {
+            Ok(content) => Some(BlogIcon { html : content }),
+            _ => None
+        }
+    }
 }
-*/
